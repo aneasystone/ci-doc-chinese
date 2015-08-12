fa8e5dcbddb915dfb0351c35c530e267e0a50bde
Hex <hex@codeigniter.org.cn>
2015-08-12 18:04:26 +0800

    update

--- source/_themes/sphinx_rtd_theme/static/js/theme.js
+++ source/_themes/sphinx_rtd_theme/static/js/theme.js
@@ -236,7 +236,7 @@ function setCookie(cname,cvalue,exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     var expires = "expires=" + d.toGMTString();
-    document.cookie = cname+"="+cvalue+"; "+expires;
+    document.cookie = cname+"="+cvalue+"; "+expires+"; path=/";
 }
 function getCookie(cname) {
     var name = cname + "=";
